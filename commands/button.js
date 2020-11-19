88aead7ff17abf6e107919012036cee967cf8c11
matt <mattclarkeruns@gmail.com>
2020-11-18 22:17:37 -0500

    Refactor ButtonCommand
    Closes: #6
    ButtonCommnand will now use the existing command pattern.
    Beware file I/O. fs.readFileSync is relative to pwd.
    Later I will create a new gateway into files.

---
+++ commands/button.js
@@ -0,0 +1,45 @@
+const CommandBase = require('./commandbase.js')
+const buttonData = require('../button.json')
+const fs = require('fs')
+const path = require('path')
+
+class ButtonCommand extends CommandBase {
+  static aliases = [
+    'button',
+  ]
+
+  constructor(message) {
+    super(message)
+
+    this.channelDestination = message.channel
+    this.user = message.user
+    this.newButtonCount = this.getNewButtonCount(this.user)
+    this.outgoingMessage = `:radio_button: ${this.newButtonCount}`
+  }
+
+  getNewButtonCount(user) {
+    const currentCount = JSON.parse(fs.readFileSync('./button.json'))[user]
+    return currentCount === undefined ? 1 : currentCount + 1
+  }
+
+  updateButtonJson(user, newNumber) {
+    // path is relative to pwd
+    // TODO Lean on something more robust
+    const rawData = fs.readFileSync('./button.json')
+    let buttonScores = JSON.parse(rawData)
+    buttonScores[user] = newNumber
+    const newRawData = JSON.stringify(buttonScores)
+    fs.writeFileSync('./button.json', newRawData)
+  }
+
+  execute(bot) {
+    this.updateButtonJson(this.user, this.newButtonCount)
+
+    bot.postMessage(
+      this.channelDestination,
+      this.outgoingMessage,
+    )
+  }
+}
+
+module.exports = ButtonCommand
