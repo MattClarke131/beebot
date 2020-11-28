67d7b5bb6a5ffd9934a33c56cd9c0a5d510eb65a
matt <mattclarkeruns@gmail.com>
2020-11-28 02:33:50 -0500

    [17] Create baseModel with read

--- models/baseModel.js
+++ models/baseModel.js
@@ -1,5 +1,47 @@
+const sqlite3 = require('sqlite3').verbose()
+const sqlite = require('sqlite');
+const dotenv = require('dotenv')
+dotenv.config()
+const DB_PATH = process.env.DB_DIR + '/test.sqlite'
+
 class BaseModel {
-  constructor() {
+  static TABLE_NAME = 'base'
+
+  constructor(params) {
+    // An id of 0 means there is no corresponding db row
+    this.id = params.id
+  }
+
+  // Alternate constructors
+  static getEmptyInstance() {
+    const id = 0
+    const params = { id }
+
+    return new BaseModel(params)
+  }
+
+  static async getInstanceFromId(id) {
+    const row = await this._getRowFromId(id)
+    return row.id === undefined ? this.getEmptyInstance() : new BaseModel(row)
+  }
+
+  // "private" static methods
+  static async _getDb() {
+    return await sqlite.open({
+      filename: DB_PATH,
+      driver: sqlite3.Database
+    })
+  }
+
+  static async _getRowFromId(id) {
+    const db = await this._getDb()
+    const row = await db.get(
+      `SELECT * FROM ${this.TABLE_NAME} WHERE id = ?`,
+      id
+    )
+    db.close()
+
+    return row === undefined ? {} : row
   }
 }
 
--- models/baseModel.spec.js
+++ models/baseModel.spec.js
@@ -1,8 +1,103 @@
-const BaseModel = require('./baseModel.js')
+const sqlite3 = require('sqlite3').verbose()
+const sqlite = require('sqlite');
+const dotenv = require('dotenv')
+dotenv.config()
+// TODO change this to use env.TEST_DB
+const DB_PATH = process.env.DB_DIR + '/test.sqlite'
 
-describe('baseModel constructor', () => {
-  const base = new BaseModel()
-  it('should return a baseModel instance', () => {
-    expect(base instanceof BaseModel).toBe(true)
+const BaseModel = require('./baseModel')
+const ROW_ID_1 = 1
+
+beforeEach(async () => {
+  // This function returns a promise, so we need to return the function result
+  return initializeTestDatabase()
+})
+
+afterEach(async () => {
+  // This function returns a promise, so we need to return the function result
+  return teardownTestDatabase()
+})
+
+const getDb = async () => {
+  return await sqlite.open({
+    filename: DB_PATH,
+    driver: sqlite3.Database
+  })
+}
+
+const initializeTestDatabase = async () => {
+  const testDatabase = await getDb()
+  const table = BaseModel.TABLE_NAME
+  await testDatabase.run(`
+    CREATE TABLE IF NOT EXISTS ${table} (
+      id        INTEGER   PRIMARY KEY   AUTOINCREMENT
+    )
+  `)
+
+  await testDatabase.close()
+}
+
+const teardownTestDatabase = async () => {
+  const testDatabase = await getDb()
+  const table = BaseModel.TABLE_NAME
+  await testDatabase.run(`
+    DROP TABLE IF EXISTS ${table}
+  `)
+  await testDatabase.close()
+}
+
+const addRow = async (params) => {
+  const db = await getDb()
+  const cols = Object.keys(params)
+  const vals = cols.map(col => params[col])
+  const placeholders = cols.map(p => '?').join(', ')
+  await db.run(
+    `INSERT INTO ${BaseModel.TABLE_NAME} (${cols}) VALUES (${placeholders})`,
+    vals
+  )
+  await db.close()
+}
+
+const readRow = async (id) => {
+  const db = await getDb()
+  const result =  await db.get(`SELECT * FROM ${BaseModel.TABLE_NAME} WHERE id = ${id}`)
+  await db.close()
+
+  return result
+}
+
+describe('static _getDb', () => {
+  it('should return an instance of a sqlite Database', async () => {
+    db = await BaseModel._getDb()
+    expect(db instanceof sqlite.Database).toBe(true)
+  })
+
+  it('should return an instance with with the correct database path', async () => {
+    db = await BaseModel._getDb()
+    expect(db.config.filename).toBe(DB_PATH)
+  })
+})
+
+describe('BaseModel static getEmptyInstance', () => {
+  it('should return an instance with empty values', () => {
+    const baseInstance = BaseModel.getEmptyInstance()
+    expect(baseInstance.id).toBe(0)
+  })
+})
+
+describe('BaseModel static getInstanceFromId', () => {
+  describe('when there is no corresponding db row', () => {
+    it('should return an instance with empty values', async () => {
+      const baseInstance = await BaseModel.getInstanceFromId(ROW_ID_1)
+      expect(baseInstance.id).toBe(0)
+    })
+  })
+  describe('when there is a corresponding db row', () => {
+    it('should return an instance with an id equal to that of the row', async () => {
+      const params = { id: ROW_ID_1 }
+      await addRow(params)
+      const baseInstance = await BaseModel.getInstanceFromId(ROW_ID_1)
+      expect(baseInstance.id).toBe(ROW_ID_1)
+    })
   })
 })
