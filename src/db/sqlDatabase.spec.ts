a08b72460e48ae65d5a1bdb7d6fc33b9faf5eb04
matt <mattclarkeruns@gmail.com>
2020-12-20 17:30:39 -0500

    Create SQLDatabase class

---
+++ src/db/sqlDatabase.spec.ts
@@ -0,0 +1,7 @@
+import SQLDatabase from './sqlDatabase'
+
+describe('SQLDatabase', () => {
+  it('should exist', () => {
+    expect(typeof SQLDatabase).toBe('function')
+  })
+})
---
+++ src/db/sqlDatabase.ts
@@ -0,0 +1,21 @@
+import Database from './databaseInterface'
+
+const DB_PATH = './sqlDatabase/beebot.sqlite'
+
+class SQLDatabase implements Database {
+ databasePath: string
+
+  constructor(databasePath: string = DB_PATH) {
+    this.databasePath = databasePath
+  }
+
+  getDb() {}
+  getRowFromId(tableName: string, id: number) {}
+  getRowsFromColVal(tableName: string, col: string, val: any) {
+    return []
+  }
+  insertRow(tableName: string, row: {[key: string]: any}) {}
+  updateRow(tableName: string, row:{[key: string]: any}) {}
+}
+
+export default SQLDatabase
