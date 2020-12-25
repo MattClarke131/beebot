7f75bb8464f4e702401e8ed90c5949f6cad766af
matt <mattclarkeruns@gmail.com>
2020-12-25 18:10:48 -0500

    Move bot constants into config files

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,43 @@
 // @ts-ignore
-import * as SlackBot from 'slackbots'
+import SlackBot from 'slackbots'
 import * as dotenv from 'dotenv'
-import * as fs from 'fs'
+dotenv.config()
 
 import commandRouter from './commandRouter'
 import SQLDatabase from './db/sqlDatabase'
+import botConfig from '../botConfig.json'
+import defaultBotConfig from '../defaultBotConfig.json'
 
-dotenv.config()
 const dbPath = process.env.NODE_ENV === 'test' ?
     process.env.TEST_DB_PATH :
     process.env.PROD_DB_PATH
 const database = new SQLDatabase(dbPath)
 
-const COMMAND_CHARACTER = "!"
-const BOT_NAME = 'beebot'
-
 const bot = new SlackBot({
   token: `${process.env.BOT_TOKEN}`,
-  name: BOT_NAME
+  name: botConfig.botName || defaultBotConfig.botName
 })
 
 bot.on('start', () => {
-  const params = {
-    // icon_emoji: ''
-  }
+  const params = {}
 
   bot.postMessageToChannel(
-    'dev-beebot',
-    '`M̸̰͔̲̍̈́̓͌́ỵ̶̳̽̇ͅ ̶͎͈̳̞̙͙̂́͑̀̆͠S̷̤̻̠̠̍͂̐w̸͓̥̰̳̰̦̺̱̋̋͐͂̚͝a̸͉̼͋̋̔r̶̢̡̗̩̰͒̅̽̿̈̚͝͝ḿ̶̧̻̹͍̊̕ ̷̜̝̹̲͐͋͜i̴̖̬̜͖̹͌̒̄̂̑ͅs̵̞̮͓͎̤̮̓̀̍͗ ̴̩͔̥̭̈́͒̃Ŗ̶̙͌̊̅͌͂͌ȇ̴̤̦̺̬͎͓̄͘b̴̹͆̆̉̿͐o̵̹̾̏͛̽͑̔͒͝r̷̫̝̯̼͕̖͚̎ǹ̸̨̛̩̺̿̇̚͠`',
+    botConfig.devChannel || defaultBotConfig.devChannel,
+    botConfig.bootMessage || defaultBotConfig.bootMessage,
     params
   )
 })
 
 bot.on('error', (err : any) => {
   console.log(err)
-  fs.appendFile('../errors.log', err, (err : any) => {
-    if (err) throw err;
-    console.log('Saved error!')
-  })
 })
 
 bot.on('message', (message : any) => {
   console.log(message)
+  const commandCharacter = botConfig.commandCharacter || defaultBotConfig.commandCharacter
   if(message.subtype === 'bot_message') {
     return
-  } else if(message.text && message.text[0] === COMMAND_CHARACTER) {
+  } else if(message.text && message.text[0] === commandCharacter) {
     handleMessage(message)
   }
 })
