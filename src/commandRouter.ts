d481506af59d451ab5be4cbfa040a980fd5ca83f
Matthew Clarke <4278212+MattClarke131@users.noreply.github.com>
2020-12-26 15:58:40 -0500

    [4] Use config files (#43)
    I created botConfig.json and defaultBotConfig.json. defaultBotConfig.json contains the "out of the box" settings for my bot. A setting defined in botConfig.json overrides default settings. If a setting is not defined in botConfig.json, the setting defined in defaultBotConfig.json is used.
    
    CommandRouter
    I refactored CommandRouter to be a class, and use config files. I injected config files into the constructor; This helped with testing the logic.

--- src/commandRouter.ts
+++ src/commandRouter.ts
@@ -2,14 +2,50 @@ import Anon from "./commands/anon"
 import Button from "./commands/button"
 import NoOpCommand from "./commands/noop"
 
-const commandRouter = (command: string) => {
-  if (Anon.aliases.includes(command)) {
-    return Anon
-  } else if (Button.aliases.includes(command)) {
-    return Button
-  } else {
-    return NoOpCommand
+const commandMap: {[key: string]: any} = {
+  // Please keep alphabetized
+  'anon': Anon,
+  'button': Button,
+  'noOpCommand': NoOpCommand,
+}
+
+class CommandRouter {
+  botConfig: { [key: string]: any }
+  defaultBotConfig: { [key: string]: any }
+
+  constructor(botConfig: object, defaultBotConfig: object) {
+    this.botConfig = botConfig
+    this.defaultBotConfig = defaultBotConfig
+  }
+
+  route(alias: string) {
+    const command: string = Object.keys(commandMap).find((key: string) => {
+      return this.commandIsEnabled(key)
+      && this.aliasIsEnabled(key, alias)
+    }) || 'noOpCommand'
+
+    return commandMap[command]
+  }
+
+  commandIsEnabled(command: string) {
+    return (
+      this.botConfig.commands[command]?.enabled
+      || (
+        !this.botConfig.commands[command]
+        && this.defaultBotConfig.commands[command]?.enabled
+      )
+    )
+  }
+
+  aliasIsEnabled(command: string, alias: string) {
+    return (
+      this.botConfig.commands[command]?.aliases?.includes(alias)
+      || (
+        !this.botConfig.commands[command]?.aliases
+        && this.defaultBotConfig.commands[command]?.aliases?.includes(alias)
+      )
+    )
   }
 }
 
-export default commandRouter
+export default CommandRouter
