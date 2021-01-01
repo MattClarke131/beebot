20f07281a3fb6beb3844434c1a38157f091108b9
matt <mattclarkeruns@gmail.com>
2020-12-31 22:03:09 -0500

    Define constructor Params interface

--- src/models/commandCall.spec.ts
+++ src/models/commandCall.spec.ts
@@ -1,7 +1,25 @@
 import CommandCall from './commandCall'
 
+const COMMAND = '!thing'
+const TEXT = '!thing arg1 arg2 arg3'
+const USER_SLACK_ID = 'ABC123'
+const TIMESTAMP = Date.now()
+
 describe('CommandCall', () => {
   it('should exist', () => {
     expect(typeof CommandCall).toBe('function')
+  })
+
+  describe('constructor', () => {
+    it('should be able to be instantiated', () => {
+      const params = {
+        command: COMMAND,
+        messageText: TEXT,
+        userSlackId: USER_SLACK_ID,
+        timestamp: TIMESTAMP,
+      }
+      const instance = new CommandCall(params)
+      expect(instance instanceof CommandCall).toBe(true)
+    })
   })
 })
--- src/models/commandCall.ts
+++ src/models/commandCall.ts
@@ -1,10 +1,29 @@
 const TABLE_NAME = 'command_call'
 
+interface Params {
+  id?: number
+  command: string
+  messageText: string
+  userSlackId: string
+  timestamp: number
+}
+
 class CommandCall {
   tableName: string
+  id: number
+  command: string
+  messageText: string
+  userSlackId: string
+  timestamp: number
 
-  constructor() {
+
+  constructor(params: Params) {
     this.tableName = TABLE_NAME
+    this.id = params.id ?? 0
+    this.command = params.command
+    this.messageText = params.messageText
+    this.userSlackId = params.userSlackId
+    this.timestamp = params.timestamp
   }
 }
 
