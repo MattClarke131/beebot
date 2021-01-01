29492bc6906e0912b8a9b4e63387f11e930e182a
matt <mattclarkeruns@gmail.com>
2021-01-01 13:08:14 -0500

    Create save method for CommandCall

--- src/models/commandCall.spec.ts
+++ src/models/commandCall.spec.ts
@@ -1,4 +1,6 @@
 import CommandCall from './commandCall'
+import MockDatabase from '../db/mockDatabase'
+import Database from '../db/databaseInterface'
 
 const COMMAND = '!thing'
 const TEXT = '!thing arg1 arg2 arg3'
@@ -12,14 +14,37 @@ describe('CommandCall', () => {
 
   describe('constructor', () => {
     it('should be able to be instantiated', () => {
-      const params = {
-        command: COMMAND,
-        messageText: TEXT,
-        userSlackId: USER_SLACK_ID,
-        timestamp: TIMESTAMP,
-      }
-      const instance = new CommandCall(params)
+      const instance = getCommandCallInstance()
       expect(instance instanceof CommandCall).toBe(true)
     })
   })
+
+  describe('save', () => {
+    it('should call database.insertRow', () => {
+      let mockDatabase = new MockDatabase()
+      mockDatabase.insertRow = jest.fn(() => {})
+      const instance = getCommandCallInstance(mockDatabase)
+
+      instance.save()
+
+      expect(mockDatabase.insertRow).toHaveBeenCalled()
+    })
+  })
+
+  it('should throw if trying to update an existing row', () => {
+    let instance: CommandCall = getCommandCallInstance()
+    instance.id = 1
+    expect(instance.save()).rejects.toMatch('CommandCall rows should not be updated')
+  })
 })
+
+const getCommandCallInstance = (database: Database = new MockDatabase) => {
+  const params = {
+    command: COMMAND,
+    messageText: TEXT,
+    userSlackId: USER_SLACK_ID,
+    timestamp: TIMESTAMP,
+  }
+
+  return new CommandCall(params, database)
+}
--- src/models/commandCall.ts
+++ src/models/commandCall.ts
@@ -1,3 +1,6 @@
+import Database from '../db/databaseInterface'
+import SQLDatabase from '../db/sqlDatabase'
+
 const TABLE_NAME = 'command_call'
 
 interface Params {
@@ -15,15 +18,36 @@ class CommandCall {
   messageText: string
   userSlackId: string
   timestamp: number
+  database: Database
 
-
-  constructor(params: Params) {
+  constructor(params: Params, database: Database = new SQLDatabase) {
     this.tableName = TABLE_NAME
     this.id = params.id ?? 0
     this.command = params.command
     this.messageText = params.messageText
     this.userSlackId = params.userSlackId
     this.timestamp = params.timestamp
+
+    this.database = database
+  }
+
+  async save() {
+    const row = this._createRowHash()
+    if(this.id === 0) {
+      this.database.insertRow(this.tableName, row)
+    } else {
+        throw('CommandCall rows should not be updated')
+    }
+  }
+
+  _createRowHash() {
+    return {
+      id: this.id,
+      command: this.command,
+      message_text: this.messageText,
+      user_slack_id: this.userSlackId,
+      timestamp: this.timestamp,
+    }
   }
 }
 
